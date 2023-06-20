d24d4876930a69c20c88e6f27a4dd73a73c323c9
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-06-20 18:30:32 +0530

    'added_punctuation_to_gold_corpus_'

--- src/word_segmentation_rules_generator/max_matcher/max_matcher.py
+++ src/word_segmentation_rules_generator/max_matcher/max_matcher.py
@@ -6,9 +6,6 @@ def botok_max_matcher(file_string):
     """
     input: string of a file before going under max match(botok)
     output/return: cleaned/preprocess string and word segmented
-    Eg:>
-    Input string :>༄༅། །རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེ-འི་ ཕྲེང་བ།
-    Output string:>  རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེའི་ ཕྲེང་བ་
     """
     file_string = replace_initial_patterns(file_string)
     t = Text(file_string)
@@ -17,5 +14,4 @@ def botok_max_matcher(file_string):
 
 
 if __name__ == "__main__":
-    # print(botok_max_matcher("༄༅། །རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེའི་ ཕྲེང་བ།"), end="")
     pass
--- src/word_segmentation_rules_generator/preprocessing/preprocessor.py
+++ src/word_segmentation_rules_generator/preprocessing/preprocessor.py
@@ -1,6 +1,6 @@
 import re
 
-from pybo.corpus.word_cleanup import word_cleanup
+from botok import Text 
 
 
 def replace_initial_patterns(file_string):
@@ -18,14 +18,10 @@ def file_2_botok(file_string):
     """
     input: string of a file before going under max match(botok)
     output/return: cleaned/preprocess string
-    *Puntuation wont be present in the output
-    (because token.text_cleaned attribute of '༄༅།' is ''), and the character wont also effect in token adjustment
-    Eg:>
-    Input string :>༄༅། །རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེ-འི་ ཕྲེང་བ།
-    Output string:> རྒྱལ་པོ་ལ་གཏམ་བྱ་བ་རིན་པོ་ཆེའི་ཕྲེང་བ་
     """
     modified_content = replace_initial_patterns(file_string)
-    preprocessed_string = word_cleanup(modified_content)
+    t = Text(modified_content)
+    preprocessed_string = t.tokenize_chunks_plaintext
     return preprocessed_string
 
 
@@ -33,24 +29,12 @@ def gold_corpus_2_tagger(file_string):
     """
     input: string where words are separated with space by human annotators before going to tagger
     output/return: cleaned/preprocess string where words are still separated by space
-    Eg:>
-    input string :>༄༅། །རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེ-འི་ ཕྲེང་བ།
-    output string:>རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེའི་ ཕྲེང་བ་
     """
     modified_content = replace_initial_patterns(file_string)
-    # This is to make sure that words such as '༄༅།' will note be included
-    file_2_botok_output = file_2_botok(file_string)
-    preprocessed_string = ""
-    words = modified_content.split()
-    for word in words:
-        cleaned_word = word_cleanup(word)
-        if cleaned_word in file_2_botok_output:
-            preprocessed_string += cleaned_word + " "
-
+    t = Text(modified_content)
+    preprocessed_string = t.tokenize_chunks_plaintext
     return preprocessed_string
 
 
 if __name__ == "__main__":
-    # print(file_2_botok("༄༅། །རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེ-འི་ ཕྲེང་བ།"))
-    # print(gold_corpus_2_tagger("༄༅། །རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེ-འི་ ཕྲེང་བ།"))
     pass
