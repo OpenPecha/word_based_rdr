d3ff6e8cd682b7d6f60de457c98b340fde39f039
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-06-21 12:33:04 +0530

    'updated_the_test_string'

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -4,7 +4,7 @@ from src.word_segmentation_rules_generator.preprocessing.preprocessor import fil
 #Test function for file string to have no gap, so that there wont be bias before sending it to botok max match
 def test_file_2_botok():
     assert (
-        file_2_botok("༄༅། །རྒྱལ་ པོ་ ལ་ གཏམ་ བྱ་ བ་ རིན་ པོ་ ཆེ འི་ ཕྲེང་་་ བ། ལ་ ལ་ ལ་ ལ་ ལ་ བ་ ཡོད། དཔལ། དགེའོ་ བཀྲ་ ཤིས་ ཤོག།")
+        file_2_botok("༄༅། །རྒྱལ་པོ་ ལ་ གཏམ་ བྱ་བ་ རིན་པོ་ཆེ འི་ ཕྲེང་་་བ། ལ་ ལ་ལ་ ལ་ ལ་བ་ ཡོད། དཔལ། དགེའོ་ བཀྲ་ཤིས་ ཤོག།")
         == "༄༅། །རྒྱལ་པོ་ལ་གཏམ་བྱ་བ་རིན་པོ་ཆེའི་ཕྲེང་་་བ། ལ་ལ་ལ་ལ་ལ་བ་ཡོད། དཔལ། དགེའོ་བཀྲ་ཤིས་ཤོག།"
     )
 
