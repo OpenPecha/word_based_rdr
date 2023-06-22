566e276c256c2aafdf86616b493dbbf9dca0854d
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-06-22 19:36:10 +0530

    'deleted_duplicate_function'

--- src/word_segmentation_rules_generator/tagger/tagger.py
+++ src/word_segmentation_rules_generator/tagger/tagger.py
@@ -33,27 +33,6 @@ def split_by_TSEK(string_to_split):
         
     splited_list = list(filter(None, splited_list))
     return splited_list
-def split_by_TSEK(string_to_split):
-    pattern = r"[་]+"  #Removing multiple TSEKs
-    replacement = '་'
-    string_to_split = re.sub(pattern, replacement, string_to_split)
-    split_pattern = TSEK
-    splited_list= re.split(split_pattern, string_to_split)
-    if len(splited_list)==1:
-        return splited_list
-    for index, element in enumerate(splited_list):
-        if element == '':
-            splited_list[index-1] += TSEK
-            break
-        if index == len(splited_list)-1:
-            break
-        if splited_list[index+1] != '':
-            splited_list[index] += TSEK
-    
-        
-    splited_list = list(filter(None, splited_list))
-    return splited_list
-
 
 def split_list_with_TSEK(list_to_split):
     specific_character = TSEK
