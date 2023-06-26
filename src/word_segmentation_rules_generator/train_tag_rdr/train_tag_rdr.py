bffa02ee750e3cb92c6b3aa8dafc0879c0af3750
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-06-26 10:55:30 +0530

    'commented_train_and_tag_rdr_functions'

--- src/word_segmentation_rules_generator/train_tag_rdr/train_tag_rdr.py
+++ src/word_segmentation_rules_generator/train_tag_rdr/train_tag_rdr.py
@@ -4,6 +4,11 @@ from ..RDRPOSTagger.pSCRDRtagger.RDRPOSTagger import run
 
 
 def train_rdr(file_to_train_tagged="TIB_tagged.txt"):
+    """
+    Input: File already tagged, output from botok and then through tagger file
+    Output: Two files i)RDR rules .RDR ii)RDR dictionary .DICT
+    Important note: File should be in the folder 'data', and output in 'resources'
+    """
     current_dir = os.path.dirname(__file__)
     relative_path = "..\\data\\" + file_to_train_tagged
     file_path = os.path.join(current_dir, relative_path)
@@ -16,6 +21,11 @@ def tag_rdr(
     RDR_rules="TIB_tagged.txt.RDR",
     RDR_dictionary="TIB_tagged.txt.DICT",
 ):
+    """
+    Input : file that is already went through botok max matched algorithm and word segmented,
+    Output: file tagged acccording to the RDR model rules and dictionary.
+    Important note: File should be in the folder 'data', and output in 'resources'
+    """
     current_dir = os.path.dirname(__file__)
     file_relative_path = "..\\data\\" + file_to_tag
     rdr_rules_relative_path = "..\\resources\\" + RDR_rules
