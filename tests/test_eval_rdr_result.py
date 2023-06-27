7ac1d001f65e5f4d53a68e4644def8c6e3c51131
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-06-27 12:49:34 +0530

    'previous_commit_continous'

--- tests/test_eval_rdr_result.py
+++ tests/test_eval_rdr_result.py
@@ -5,7 +5,7 @@ from src.word_segmentation_rules_generator.eval_rdr_result.eval_rdr_result impor
 
 def test_eval_rdr_result():
     result_value = eval_rdr_result(
-        goldStandardCorpus="TIB_train_maxmatched_tagged.txt",
-        taggedCorpus="TIB_train_maxmatched.txt.TAGGED",
+        goldStandardCorpus="TIB_short_test_maxmatched_tagged.txt",
+        taggedCorpus="TIB_short_test_maxmatched.txt.TAGGED",
     )
     assert isinstance(result_value, float), "The value is not a float"
