55a24ec971d54761aa9782e2070f4f3413ef1b62
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-06-27 21:24:04 +0530

    'added_rdr_2_cql_functionality_imported_from_pybo'

---
+++ src/word_segmentation_rules_generator/rdr_2_cql/rdr_2_cql.py
@@ -0,0 +1,22 @@
+import os
+
+from pybo.rdr.rdr_2_replace_matcher import rdr_2_replace_matcher
+
+
+def rdr_2_cql(rdr_rules_file="TIB_train_maxmatched_tagged.txt.RDR"):
+    current_dir = os.path.dirname(__file__)
+    relative_path = "../resources/" + rdr_rules_file
+    file_path = os.path.join(current_dir, relative_path)
+
+    # rdr_rules = file_path.read_text(encoding="utf-8")
+    # print(rdr_rules)
+    # rules = rdr_2_replace_matcher(rdr_rules)
+    # #print(rules)
+    with open(file_path, encoding="utf-8") as file:
+        rdr_rules = file.read()  # Read the entire file content
+        cql_rules = rdr_2_replace_matcher(rdr_rules)
+        current_dir = os.path.dirname(__file__)
+        cql_file_relative_path = "../resources/" + "CQL_rules.txt"
+        cql_file_path = os.path.join(current_dir, cql_file_relative_path)
+        with open(cql_file_path, "w", encoding="utf-8") as file:
+            file.write(cql_rules)  # Write content to the file
