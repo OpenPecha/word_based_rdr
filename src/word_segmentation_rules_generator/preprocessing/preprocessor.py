1c17a44df6fdb35295021ceab3855df1533c94a0
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-06-29 02:05:01 +0530

    'updated_preprocessor.py_for_proper_underscore_updated_test_files'

--- src/word_segmentation_rules_generator/preprocessing/preprocessor.py
+++ src/word_segmentation_rules_generator/preprocessing/preprocessor.py
@@ -6,41 +6,45 @@ from botok import TSEK
 def replace_initial_patterns(file_string, is_gold_corpus=False):
     # Some signs(i.e ?,+,- ) are presented in human annotated training files which needs to be edited
     # There are two different kind of TSEK, and here proper tsek been replaced
-    initial_patterns = {"?": " ", "+": "", "-": "", "[ ]+": " ", "༌":TSEK}
+    initial_patterns = {"?": " ", "+": "", "-": "", "༌": TSEK}
     if is_gold_corpus:
-            initial_patterns = {"?": " ", "+": "", "[ ]+": " ", "༌":TSEK}
+        initial_patterns = {"?": " ", "+": "", "༌": TSEK}
 
     modified_content = re.sub(
         "|".join(re.escape(key) for key in initial_patterns.keys()),
         lambda match: initial_patterns[match.group(0)],
         file_string,
     )
+    modified_content = adjust_spaces(modified_content)
     return modified_content
+
 
 def adjust_spaces(file_string):
     pattern = r"[ ]+"
-    replacement = ' '
+    replacement = " "
     modified_string = re.sub(pattern, replacement, file_string.strip())
     return modified_string
 
+
 def adjust_spaces_for_non_affix(file_string):
-    '''
-    Sometimes there error in gold corpus i.e 
+    """
+    Sometimes there error in gold corpus i.e
     string: ད གེ འོ་ བཀྲ་ཤིས་ ཤོག།
     Expected: དགེ འོ་ བཀྲ་ཤིས་ ཤོག།
-    *Note that in དགེ འོ་, space before འོ་ is not closed,because this is an affix 
-    '''
+    *Note that in དགེ འོ་, space before འོ་ is not closed,because this is an affix
+    """
     pattern = r"([^་།_]) ([^ར ས འི འམ འང འོ འིའོ འིའམ འིའང འོའམ འོའང ། _])"
     replacement = r"\1\2"
     modified_string = re.sub(pattern, replacement, file_string)
     return modified_string
 
+
 def adjust_spaces_for_affix(file_string):
-    '''
+    """
     Somtimes there error in gold corpus i.e
-    String: །འཁོར་བ འི་ འབྲོག་ ནི་ མི་ བཟད་པ-འི། 
-    Expected string: །འཁོར་བ-འི་ འབྲོག་ ནི་ མི་ བཟད་པ-འི། 
-    '''
+    String: །འཁོར་བ འི་ འབྲོག་ ནི་ མི་ བཟད་པ-འི།
+    Expected string: །འཁོར་བ-འི་ འབྲོག་ ནི་ མི་ བཟད་པ-འི།
+    """
     pattern = r"([^་།_]) (ར|ས|འི|འམ|འང|འོ|འིའོ|འིའམ|འིའང|འོའམ|འོའང)"
     replacement = r"\1-\2"
     modified_string = re.sub(pattern, replacement, file_string)
@@ -54,16 +58,9 @@ def file_2_botok(file_string):
     """
 
     modified_content = replace_initial_patterns(file_string)
-    #making sure there only one space
-    pattern = r"[ ]+"
-    replacement = ' '
-    modified_content = re.sub(pattern, replacement, modified_content)
-    
-    #Joining all the words, not leaving spaces unless its for SHAD
-    patterns = {
-      #  r'([^།]) ([^།])':r'\1\2', 
-      r'(?<=([^།])) (?=([^།]))':''
-    }
+
+    # Joining all the words, not leaving spaces unless its for SHAD
+    patterns = {r"(?<=([^།])) (?=([^།]))": ""}
 
     for pattern, replacement in patterns.items():
         modified_content = re.sub(pattern, replacement, modified_content)
@@ -76,27 +73,24 @@ def gold_corpus_2_tagger(file_string):
     output/return: cleaned/preprocess string where words are still separated by space
     """
     modified_content = replace_initial_patterns(file_string, is_gold_corpus=True)
+
     patterns = {
-          '([^༅])།':r'\1 །', 
-          '།\\s*།':'།_། ',
-          '([^_༅])། ': r'\1 །_ ',# དུ། -> དུ །_ 
-          ' །([^_])': r' །_ \1', # །བསྲེགས ->  །_ བསྲེགས་
-          #'([^།་༌_ ]) ': r'\1 ',  # Putting full stop དུ། -> དུ་ །_,
-          r"(་)\s*(།) ":  r"་\1 \2_ ", #གང་ །->གང་ །_
-          ' །$':' །_'
+        "།[ ]+༄": "།_༄",  # ཕྲེང་བ།  ༄༅༅།-> ཕྲེང་བ།_༄༅༅།
+        "༅[ ]+།": "༅_།",
+        "(?<=།) (?=།)": "_",  # ༄༅༅། ། ། །རྒྱ་གར་ སྐད་དུ། -> ༄༅༅།_།_།_།རྒྱ་གར་ སྐད་དུ།
+        "[ ]+།": "_།",  # རྣམ་གྲོལ་ ཞིང༌ ། ། ->རྣམ་གྲོལ་ ཞིང་ _།_།,
+        "།[ ]+": "།_",
+        r"(?<![༅།_])([།_]+)": r" \1",  # སྐད་དུ།_རཱ་ -> སྐད་དུ །_རཱ་
+        r"([།_]+)(?![༄།_])": r"\1 ",  # སྐད་དུ།_རཱ་ - >སྐད་དུ།_ རཱ་
     }
     for pattern, replacement in patterns.items():
         modified_content = re.sub(pattern, replacement, modified_content)
-    #making sure there only one space
-    pattern = r"[ ]+"
-    replacement = ' '
-    gold_corpus_output = re.sub(pattern, replacement, modified_content)
-    gold_corpus_output = adjust_spaces(gold_corpus_output)
+    gold_corpus_output = adjust_spaces(modified_content)
     gold_corpus_output = adjust_spaces_for_non_affix(gold_corpus_output)
     gold_corpus_output = adjust_spaces_for_affix(gold_corpus_output)
-    
+
     return gold_corpus_output
 
-    
+
 if __name__ == "__main__":
     pass
