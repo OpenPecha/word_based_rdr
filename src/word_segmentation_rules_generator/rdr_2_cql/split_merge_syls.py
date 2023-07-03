60743b2c0203d6b81e3a32481325f86130caeb18
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-07-04 01:57:10 +0530

    'created_separater_function_for_adjust_affix_with_tag'

--- src/word_segmentation_rules_generator/rdr_2_cql/split_merge_syls.py
+++ src/word_segmentation_rules_generator/rdr_2_cql/split_merge_syls.py
@@ -4,7 +4,7 @@ import re
 from src.word_segmentation_rules_generator.tagger.tagger import split_by_TSEK
 
 
-def split_merge_syls(tagged_file="TIB_train_maxmatched.txt.TAGGED"):
+def split_into_word_tag_list(tagged_file="TIB_test_maxmatched.txt.TAGGED"):
     """
     Input: file that is tagged by rdr rules
     Output:
@@ -17,11 +17,7 @@ def split_merge_syls(tagged_file="TIB_train_maxmatched.txt.TAGGED"):
     contents = file.read()
     words_list = contents.split()
     word_tag_list = []
-    counter = 0
     for word in words_list:
-        counter += 1
-        if counter > 100:
-            break
         word_tag_splited = word.split("/")
         if word_tag_splited[1] == "P":
             # If there is an affix involved,then there needs to be a space
@@ -31,10 +27,44 @@ def split_merge_syls(tagged_file="TIB_train_maxmatched.txt.TAGGED"):
             word_tag_list.append(word_tag_splited)
         else:
             syls_list = split_by_TSEK(word_tag_splited[0])
+            # ['ལོག་པ-འོ', 'C'], in cases like here where there are two syllables in a word but only one tag
             if len(syls_list) != len(word_tag_splited[1]):
-                print(
-                    "Error in the resulted tagged file, number of syllables is not equal to number of tags."
+                anomaly_word_tag_list = adjust_anomaly_tagged(
+                    syls_list, word_tag_splited
                 )
-            for i in range(len(syls_list)):
-                word_tag_list.append([syls_list[i], word_tag_splited[1][i]])
+                word_tag_list = word_tag_list + anomaly_word_tag_list
+            else:
+                for i in range(len(syls_list)):
+                    word_tag_list.append([syls_list[i], word_tag_splited[1][i]])
+    word_tag_list = adjust_affix_with_tag(word_tag_list)
     print(word_tag_list)
+
+
+def adjust_anomaly_tagged(syls_list, word_tag_splited):
+    anomaly_word_tag_list = []
+    for i in range(len(syls_list)):
+        if i < len(word_tag_splited[1]):
+            anomaly_word_tag_list.append([syls_list[i], word_tag_splited[1][i]])
+        else:
+            anomaly_word_tag_list.append([syls_list[i], "C"])
+    return anomaly_word_tag_list
+
+
+def adjust_affix_with_tag(word_tag_list):
+    for i in range(len(word_tag_list)):
+        if word_tag_list[i][1] in ["P", "N", "C"]:
+            # If there is an affix involved,then there needs to be a space
+            pattern = "-"
+            replacement = " -"
+            word_tag_list[i][0] = re.sub(pattern, replacement, word_tag_list[i][0])
+        else:  # So the tag is eithe A or B
+            pattern = "-"
+            replacement = " -"
+            match = re.search(pattern, word_tag_list[i][0])
+            if match:
+                word_tag_list[i][0] = re.sub(pattern, replacement, word_tag_list[i][0])
+            else:
+                pattern = r"(ར|ས|འི|འམ|འང|འོ|འིའོ|འིའམ|འིའང|འོའམ|འོའང)"
+                replacement = r" -\1"
+                word_tag_list[i][0] = re.sub(pattern, replacement, word_tag_list[i][0])
+    return word_tag_list
