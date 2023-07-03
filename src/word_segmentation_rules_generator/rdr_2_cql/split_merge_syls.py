f87e7f2c55cb421a37ea1d06c59e5af4d4ab053d
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-07-03 22:03:00 +0530

    'splited_word_that_are_not_tagged_P_into_syls_and_tag'

--- src/word_segmentation_rules_generator/rdr_2_cql/split_merge_syls.py
+++ src/word_segmentation_rules_generator/rdr_2_cql/split_merge_syls.py
@@ -1,5 +1,7 @@
 import os
 
+from src.word_segmentation_rules_generator.tagger.tagger import split_by_TSEK
+
 
 def split_merge_syls(tagged_file="TIB_train_maxmatched.txt.TAGGED"):
     """
@@ -14,8 +16,20 @@ def split_merge_syls(tagged_file="TIB_train_maxmatched.txt.TAGGED"):
     contents = file.read()
     words_list = contents.split()
     word_tag_list = []
+    counter = 0
     for word in words_list:
+        counter += 1
+        if counter > 100:
+            break
         word_tag_splited = word.split("/")
-        word_tag_list.append(word_tag_splited)
-
+        if word_tag_splited[1] == "P":
+            word_tag_list.append(word_tag_splited)
+        else:
+            syls_list = split_by_TSEK(word_tag_splited[0])
+            if len(syls_list) != len(word_tag_splited[1]):
+                print(
+                    "Error in the resulted tagged file, number of syllables is not equal to number of tags."
+                )
+            for i in range(len(syls_list)):
+                word_tag_list.append([syls_list[i], word_tag_splited[1][i]])
     print(word_tag_list)
