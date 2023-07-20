3f6daecc7388d33812b9f5bb67ad67d2bf6f31be
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-07-20 15:19:07 +0530

    'added_file_name_for_cql_rules_arg_rdr_2_cql.py'

--- src/word_segmentation_rules_generator/rdr_2_cql/rdr_2_cql.py
+++ src/word_segmentation_rules_generator/rdr_2_cql/rdr_2_cql.py
@@ -3,7 +3,10 @@ import os
 from pybo.rdr.rdr_2_replace_matcher import rdr_2_replace_matcher
 
 
-def rdr_2_cql(rdr_rules_file="TIB_train_maxmatched_tagged.txt.RDR"):
+def rdr_2_cql(
+    rdr_rules_file="TIB_train_maxmatched_tagged.txt.RDR",
+    new_cql_file_name="TIB_train_CQL_rules_temp.txt",
+):
     current_dir = os.path.dirname(__file__)
     relative_path = "../resources/" + rdr_rules_file
     file_path = os.path.join(current_dir, relative_path)
@@ -14,7 +17,7 @@ def rdr_2_cql(rdr_rules_file="TIB_train_maxmatched_tagged.txt.RDR"):
         # in the code rdr_2_replace_matcher, there been done pos but here we will need word tag
         cql_rules = cql_rules.replace("pos", "word_tag")
         current_dir = os.path.dirname(__file__)
-        cql_file_relative_path = "../resources/" + "TIB_train_CQL_rules.txt"
+        cql_file_relative_path = "../resources/" + new_cql_file_name
         cql_file_path = os.path.join(current_dir, cql_file_relative_path)
         with open(cql_file_path, "w", encoding="utf-8") as file:
             file.write(cql_rules)  # Write content to the file
