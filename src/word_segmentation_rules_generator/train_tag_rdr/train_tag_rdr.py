1f0cbe10332ac7cd90a9bd7e611eb51c364b22fe
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-07-20 14:27:15 +0530

    'new_train_with_ExtPOSTagger_function_in_train_tag_rdr_with_threshold_arg'

--- src/word_segmentation_rules_generator/train_tag_rdr/train_tag_rdr.py
+++ src/word_segmentation_rules_generator/train_tag_rdr/train_tag_rdr.py
@@ -1,5 +1,6 @@
 import os
 
+from ..RDRPOSTagger.pSCRDRtagger.ExtRDRPOSTagger import ExtRDR_RUN
 from ..RDRPOSTagger.pSCRDRtagger.RDRPOSTagger import run
 
 
@@ -14,6 +15,31 @@ def train_rdr(file_to_train_tagged="TIB_train_maxmatched_tagged.txt"):
     file_path = os.path.join(current_dir, relative_path)
     function_arguments = ["RDRPOSTagger.py", "train", file_path]
     run(function_arguments)
+
+
+def train_with_external_rdr(gold_corpus_file, external_tagged_file, THRESHOLD=(1, 1)):
+
+    """
+    Input: Gold standard corpus, tagged file by external tagger, threshold
+    Output: Two files i)RDR rules .RDR
+    """
+    current_dir = os.path.dirname(__file__)
+    gold_corpus_relative_path = "../data/" + gold_corpus_file
+    gold_corpus_file_path = os.path.join(current_dir, gold_corpus_relative_path)
+
+    external_tagged_file_relative_path = "../data/" + external_tagged_file
+    external_tagged_file_path = os.path.join(
+        current_dir, external_tagged_file_relative_path
+    )
+
+    function_arguments = [
+        "ExtRDRPOSTagger.py",
+        "train",
+        gold_corpus_file_path,
+        external_tagged_file_path,
+        THRESHOLD,
+    ]
+    ExtRDR_RUN(function_arguments)
 
 
 def tag_rdr(
