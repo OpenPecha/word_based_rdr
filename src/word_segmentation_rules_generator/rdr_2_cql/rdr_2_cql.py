90379309875ea262fbef8117285f8c40c88ec05e
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-07-20 15:50:01 +0530

    'new_funct_apppend_new_cql_rules_to_main_in_rdr2cql.py'

--- src/word_segmentation_rules_generator/rdr_2_cql/rdr_2_cql.py
+++ src/word_segmentation_rules_generator/rdr_2_cql/rdr_2_cql.py
@@ -21,3 +21,27 @@ def rdr_2_cql(
         cql_file_path = os.path.join(current_dir, cql_file_relative_path)
         with open(cql_file_path, "w", encoding="utf-8") as file:
             file.write(cql_rules)  # Write content to the file
+
+
+def append_new_cql_rules_to_main(source_file, destination_file):
+    """
+    Used to append the replace cql rules of minimum cases to the main file
+    """
+    current_dir = os.path.dirname(__file__)
+    source_relative_path = "../resources/" + source_file
+    source_file_path = os.path.join(current_dir, source_relative_path)
+
+    destination_relative_path = "../resources/" + destination_file
+    destination_file_path = os.path.join(current_dir, destination_relative_path)
+
+    try:
+        with open(source_file_path) as source:
+            lines_to_append = source.readlines()
+        with open(destination_file_path, "a") as destination:
+            for line in lines_to_append:
+                destination.write(line)
+        print("new cql rules successfully appended to the destination file.")
+    except FileNotFoundError:
+        print("One of the files does not exist.")
+    except Exception as e:
+        print("An error occurred:", str(e))
