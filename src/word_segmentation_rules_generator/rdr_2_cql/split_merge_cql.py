6a274c8a045a7edd400010283e4670f833e5fcfe
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-07-24 20:51:55 +0530

    'function_split_tag_list_with_index_for_further_split_merge'

--- src/word_segmentation_rules_generator/rdr_2_cql/split_merge_cql.py
+++ src/word_segmentation_rules_generator/rdr_2_cql/split_merge_cql.py
@@ -17,17 +17,22 @@ def make_cql_rule(start_index, i, word_list, tag_list):
     pass
 
 
-def split_merge_cql(tagged_string):
-    word_list, tag_list = tagged_string_to_word_tag_list(tagged_string)
+def split_tag_list_with_index(tag_list):
+    tag_split_list = []
     start_index = -1
     for i in range(len(tag_list)):
         if tag_list[i] == "P" and start_index == -1:
             continue
         if tag_list[i] == "P" and start_index != -1:
-            make_cql_rule(start_index, i, word_list, tag_list)
+            tag_split_list.append((start_index, i - 1))
             start_index = -1
-        if start_index == -1:
+            continue
+        if start_index == -1 and tag_list[i] != "P":
             start_index = i
-        if "C" in tag_list[i] or "B" in tag_list[i]:
-            make_cql_rule(start_index, i, word_list, tag_list)
-            start_index = -1
+    return tag_split_list
+
+
+def split_merge_cql(tagged_string):
+    word_list, tag_list = tagged_string_to_word_tag_list(tagged_string)
+    tag_split_list = split_tag_list_with_index(tag_list)
+    return tag_split_list
