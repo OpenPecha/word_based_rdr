9ea4943f9de8bc248ac1c32c6910dd90a2ff4ef8
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-07-24 19:56:42 +0530

    'function_split_merge_cql_for_indicating_words_for_split_merge'

--- src/word_segmentation_rules_generator/rdr_2_cql/split_merge_cql.py
+++ src/word_segmentation_rules_generator/rdr_2_cql/split_merge_cql.py
@@ -11,3 +11,23 @@ def tagged_string_to_word_tag_list(tagged_string):
         tag_list.append(tag)
 
     return word_list, tag_list
+
+
+def make_cql_rule(start_index, i, word_list, tag_list):
+    pass
+
+
+def split_merge_cql(tagged_string):
+    word_list, tag_list = tagged_string_to_word_tag_list(tagged_string)
+    start_index = -1
+    for i in range(len(tag_list)):
+        if tag_list[i] == "P" and start_index == -1:
+            continue
+        if tag_list[i] == "P" and start_index != -1:
+            make_cql_rule(start_index, i, word_list, tag_list)
+            start_index = -1
+        if start_index == -1:
+            start_index = i
+        if "C" in tag_list[i] or "B" in tag_list[i]:
+            make_cql_rule(start_index, i, word_list, tag_list)
+            start_index = -1
