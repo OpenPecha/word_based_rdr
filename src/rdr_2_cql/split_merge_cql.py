40673626186d372ac786761e3a26b39075325b87
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-08-30 08:56:30 +0530

    'Extract_rdr_condition_conlusion_from_rdr_string'

--- src/rdr_2_cql/split_merge_cql.py
+++ src/rdr_2_cql/split_merge_cql.py
@@ -1,15 +1,28 @@
+import sys
 from pathlib import Path
 
-from rdr_2_replace_matcher import find_levels, find_rules
+# Add the root directory of your project to sys.path
+root_path = (
+    Path(__file__).resolve().parents[2]
+)  # Adjust the number of parents as needed
+sys.path.append(str(root_path))
+
+from src.rdr_2_cql.rdr_2_replace_matcher import find_levels, find_rules  # noqa
 
 
 def split_merge_cql(rdr_string):
-    rdr_format = find_rules(find_levels(rdr_string))
-    return rdr_format
+    rdr_rules = find_rules(find_levels(rdr_string))
+    for rdr_rule in rdr_rules:
+        rdr_condition = rdr_rule["test"]
+        rdr_conclusion = rdr_rule["ccl"][0][0][1]  # noqa
+
+        # Checking for Tag intro (not an actual rule)
+        # 	object.tag == "U" : object.conclusion = "U" (looks like this in the .RDR)
+        if len(rdr_condition) == 1 and len(rdr_condition[0]) == 1:
+            continue
+    return rdr_rules
 
 
 if __name__ == "__main__":
     rdr_string = Path("src/data/rdr_rules.txt").read_text(encoding="utf-8")
-    result = split_merge_cql(rdr_string)
-    for r in result:
-        print(r["test"])
+    rdr_rules = split_merge_cql(rdr_string)
