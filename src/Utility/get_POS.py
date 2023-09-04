3b9efed250f4c0bbfa91846e97bacbceb20238ba
Tenzin <tenzintsunduebhattu@gmail.com>
2023-09-04 14:40:36 +0530

    'Code_function_GetPosThroughWordTokenizer_for_word_with_multiple_senses'

--- src/Utility/get_POS.py
+++ src/Utility/get_POS.py
@@ -1,6 +1,7 @@
 import re
 from pathlib import Path
 
+from botok import WordTokenizer
 from botok.config import Config
 from botok.textunits.bosyl import BoSyl
 from botok.tries.trie import Trie
@@ -59,6 +60,43 @@ def get_POS(word_string):
     return NO_POS
 
 
+def get_POS_through_Word_Tokenizer(string):
+    config = Config(dialect_name="general", base_path=Path.home())
+    wt = WordTokenizer(config=config)
+    token_list = wt.tokenize(string, split_affixes=False)
+    if len(token_list) == 1:
+        token = token_list[0]
+        token_senses = token["senses"]
+        unique_token_senses = list(set(token_senses))
+        token_lemma_POS = get_POS(token["lemma"])
+
+        filtered_token_senses = [
+            x for x in unique_token_senses if x["pos"] != token_lemma_POS
+        ]
+
+        max_freq = -1
+        word_pos = ""
+        for curr_token_sense in filtered_token_senses:
+            if curr_token_sense.get("freq", 0) == 0 and max_freq == -1:
+                max_freq = 1
+                word_pos = curr_token_sense["pos"]
+                continue
+            if curr_token_sense.get("freq", 0) == 0:
+                continue
+            if curr_token_sense["freq"] > max_freq:
+                max_freq = curr_token_sense["freq"]
+                word_pos = curr_token_sense["pos"]
+        return word_pos
+    return get_POS("string")
+
+
 if __name__ == "__main__":
-    word_pos = get_POS("མངོན་པར་")
+    word_pos = get_POS("ཆོས་")
     print(word_pos)
+
+    word_pos = get_POS("ཆོ་")
+    print(word_pos)
+    word_pos = get_POS_through_Word_Tokenizer("ཆོས་")
+    word_pos = get_POS_through_Word_Tokenizer("ཆོ་")
+
+    # print(word_pos)
