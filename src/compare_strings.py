a898968e1946d052f66732b61437e7a52af78f30
Tenzin <tenzintsunduebhattu@gmail.com>
2023-09-25 19:43:47 +0530

    'Update_refactor_compare_strings'

--- src/compare_strings.py
+++ src/compare_strings.py
@@ -3,24 +3,21 @@ from .tokenizer_pipeline import botok_word_tokenizer_pipeline
 from .Utility.regex_replacer import replace_with_regex
 
 
-def is_corpus_tokenization_identical(gold_corpus_string, botok_output_string):
+def is_corpus_tokenization_identical(gold_corpus, tokenized_output):
+    # here spaces are replaced with underscore to compare if syllables are separated correctly
     pattern = {" ": "_", "-": "", "_": ""}
-    gold_corpus_without_spaces = replace_with_regex(pattern, gold_corpus_string)
-    botok_output_string_without_spaces = replace_with_regex(
-        pattern, botok_output_string
-    )
+    gold_corpus = replace_with_regex(pattern, gold_corpus)
+    botok_output = replace_with_regex(pattern, tokenized_output)
 
-    equal_string_length = len(gold_corpus_without_spaces) == len(
-        botok_output_string_without_spaces
-    )
-    return equal_string_length
+    is_syllables_separated_correctly = len(gold_corpus) == len(botok_output)
+    return is_syllables_separated_correctly
 
 
 def compare_gold_corpus_and_tokenized_output(gold_corpus):
-    gold_corpus_output = transform_gold_corpus_for_tagging(gold_corpus)
-    botok_output = botok_word_tokenizer_pipeline(gold_corpus)
+    gold_corpus_cleaned = transform_gold_corpus_for_tagging(gold_corpus)
+    tokenized_output = botok_word_tokenizer_pipeline(gold_corpus)
 
-    equal_number_of_syls = is_corpus_tokenization_identical(
-        gold_corpus_output, botok_output
+    is_syllables_separated_correctly = is_corpus_tokenization_identical(
+        gold_corpus_cleaned, tokenized_output
     )
-    return equal_number_of_syls, gold_corpus_output, botok_output
+    return is_syllables_separated_correctly, gold_corpus_cleaned, tokenized_output
