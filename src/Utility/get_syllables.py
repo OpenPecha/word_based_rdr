2b036e845a0bed6a37ef743efda6d98d6e9fae6c
Tenzin <tenzintsunduebhattu@gmail.com>
2023-09-25 20:37:14 +0530

    'Update_refactor_function_get_syllables'

--- src/Utility/get_syllables.py
+++ src/Utility/get_syllables.py
@@ -5,27 +5,18 @@ from botok import TSEK
 
 
 def filter_multiple_tsek(text: str) -> str:
-    pattern = r"[་]+"  # Removing multiple TSEKs
-    replacement = "་"
-    text = re.sub(pattern, replacement, text)
+    # Replace multiple consecutive TSEK characters with a single TSEK
+    text = re.sub(r"་+", "་", text)
     return text
 
 
 def get_syllables(text: str) -> List[str]:
     text_to_split = filter_multiple_tsek(text)
-    text_syllables = re.split(TSEK, text_to_split)
-    if len(text_syllables) == 1:
-        return text_syllables
-    for index, element in enumerate(text_syllables):
-        if element == "":
-            text_syllables[index - 1] += TSEK
-            break
-        if index == len(text_syllables) - 1:
-            break
-        if text_syllables[index + 1] != "":
-            text_syllables[index] += TSEK
+    tsek_split_text = re.split(TSEK, text_to_split)
 
-    text_syllables = list(filter(None, text_syllables))
+    text_syllables = [syl + TSEK for syl in tsek_split_text[:-1] if syl != ""]
+    if tsek_split_text[-1] != "":
+        text_syllables.append(tsek_split_text[-1])
     return text_syllables
 
 
@@ -37,4 +28,4 @@ def get_syllables_without_tsek(text: str) -> List[str]:
 
 
 if __name__ == "__main__":
-    print(get_syllables("ལ-ས་པ་"))
+    print(get_syllables("ལས་པ་"))
