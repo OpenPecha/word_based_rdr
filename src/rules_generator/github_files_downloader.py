43d446ae7bf57f07f414f97cb26207a3b7a54762
Tenzin <tenzintsunduebhattu@gmail.com>
2023-10-12 17:15:20 +0530

    FEAT_function_download_file_with_url

--- src/rules_generator/github_files_downloader.py
+++ src/rules_generator/github_files_downloader.py
@@ -1,9 +1,14 @@
-from github import Github
+from pathlib import Path
 
-from rules_generator.github_token import GITHUB_TOKEN
+import requests
+from github import Github
+from retrying import retry
+
+from rules_generator.github_token import GITHUB_TOKEN  # noqa
 
 REPO_OWNER = "OpenPecha"
 REPO_NAME = "word-segmentation-data"
+RESOURCES_DIR = Path(__file__).resolve().parent.parent.parent / "resources"
 
 
 class GitHubFileDownloader:
@@ -35,8 +40,35 @@ class GitHubFileDownloader:
             return []
 
 
+@retry(
+    stop_max_attempt_number=3,  # Maximum number of retries
+    wait_fixed=2000,  # Delay between retries in milliseconds (2 seconds)
+    retry_on_exception=lambda x: isinstance(
+        x, requests.exceptions.RequestException
+    ),  # Retry on network errors
+)
+def download_file_with_url(
+    download_url, new_downloaded_file_name, destination_folder=RESOURCES_DIR
+):
+
+    if download_url is None:
+        print("Download URL is None")
+        return
+    # Send a GET request to download the file
+    response = requests.get(download_url)
+
+    new_downloaded_file_name = new_downloaded_file_name
+    local_file_path = Path(destination_folder) / new_downloaded_file_name
+    if response.status_code == 200:
+        # Open the local file and save the downloaded content
+        with open(local_file_path, "wb") as local_file:
+            local_file.write(response.content)
+        print(f"File downloaded and saved to {local_file_path}")
+    else:
+        print(
+            f"Failed to download file {new_downloaded_file_name}. Status code: {response.status_code}"
+        )
+
+
 if __name__ == "__main__":
-    file_downloader = GitHubFileDownloader(GITHUB_TOKEN, REPO_OWNER, REPO_NAME)
-    json_urls = file_downloader.get_json_file_download_urls_from_repo()
-    for url in json_urls:
-        print(url)
+    pass
